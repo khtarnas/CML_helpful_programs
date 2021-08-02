eec671ed6f5e651c2c343fd191e4f48db21e12fb
Hokua <60975580+Hokua@users.noreply.github.com>
2021-08-02 11:59:00 -0400

    bug fixed where command added to .bash_profile was wrong because of a missing '\' behind the '$'

--- Run_Live_Setup/create_run_live.sh
+++ Run_Live_Setup/create_run_live.sh
@@ -4,14 +4,14 @@
 LOCATION=$(pwd)
 FILE=".bash_profile"
 INPUT="run_live () {\n
-LOC=$(pwd)\n
+LOC=\$(pwd)\n
 psiturk server on\n
 text=\$(psiturk debug -p)\n
 cd ~/.cml_files\n
 javac ReplaceText.java\n
 text=\$(java ReplaceText \"\$text\" debug live)\n
 echo \"\$text\" | tail -n1 | pbcopy\n
-cd $LOC\n
+cd \$LOC\n
 }"
 
 # Check file and input alias
